c6f891c90f8d5153350a01272984ef2417dbc11a
Bastin-Thomas <thomas.bastin@student.hepl.be>
2022-09-23 21:17:52 +0200

    :bug: fix source conflict

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,10 @@
 #include <iostream>
 #include <vector>
 
-#include "./Film/film.h"
+#include "./Film/film.hpp"
 
 using namespace std;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 09137a01d736121c82fa6b30fd3a55634a993c1f
 int main(){
     string fileName;
     string line;
@@ -25,22 +21,8 @@ int main(){
     csv.open(fileName);
 
     while(getline(csv, line)){
-<<<<<<< HEAD
         Film tmp;
         tmp = Film(line);
-=======
-        Film tmp = Film(line);
-
-        //Creation des premiers Tokens, récupérer en vecteur
-        tokens = tmp.getTokens(line, L'‣');
-
-        //Lecture des premiers Tokens, pour les distribués en Type Film
-        for(int i = 0; i < tokens.size() ; i++){
-            cout << tokens[i] << ",  ";
-        }       
-
-        cout << endl;
->>>>>>> 09137a01d736121c82fa6b30fd3a55634a993c1f
     }
     return 0;
 }
