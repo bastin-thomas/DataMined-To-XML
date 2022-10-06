bbc301a0ee1a2cc96131a5de006e6d5c38d0719d
Thomas Bastin <thomas.bastin@student.hepl.be>
2022-10-06 13:49:46 +0200

    :sparkles: DTD creation

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,37 @@ int main(){
     }
 
     xml << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>" << endl;
+    xml << "<!DOCTYPE Films [" <<endl;
+    xml << "<!ELEMENT Films (Film*)>"<<endl;
+    xml << "\t<!ELEMENT Film (Title, Time?, Status, Vote, RunTime, Certification, Budget, Tag, Genres, Directors, Actors)>"<<endl;
+    xml << "\t\t<!ATTLIST Film Id CDATA #REQUIRED>"<<endl;
+    xml << "\t\t<!ATTLIST Film PosterPath CDATA #REQUIRED>"<<endl;
+    xml << "\t<!ELEMENT Title (#PCDATA)>"<<endl;
+    xml << "\t\t<!ATTLIST Title OriginalTitle CDATA #REQUIRED>"<<endl;
+    xml << "\t<!ELEMENT Time EMPTY>"<<endl;
+    xml << "\t\t<!ATTLIST Time Day CDATA #REQUIRED>"<<endl;
+	xml << "\t\t<!ATTLIST Time Month CDATA #REQUIRED>"<<endl;
+    xml << "\t\t<!ATTLIST Time Year CDATA #REQUIRED>"<<endl;
+    xml << "\t<!ELEMENT Status (#PCDATA)>"<<endl;
+    xml << "\t<!ELEMENT Vote (#PCDATA)>"<<endl;
+    xml << "\t\t<!ATTLIST Vote Count CDATA #REQUIRED>"<<endl;
+    xml << "\t<!ELEMENT RunTime (#PCDATA)>"<<endl;
+    xml << "\t<!ELEMENT Certification (#PCDATA)>"<<endl;
+    xml << "\t<!ELEMENT Budget (#PCDATA)>"<<endl;
+    xml << "\t<!ELEMENT Tag (#PCDATA)>"<<endl;
+    xml << "\t<!ELEMENT Genres (Genre*)>"<<endl;
+    xml << "\t\t<!ELEMENT Genre EMPTY>"<<endl;
+    xml << "\t\t\t<!ATTLIST Genre Id CDATA #REQUIRED>" << endl;
+    xml << "\t\t\t<!ATTLIST Genre Label CDATA #REQUIRED>" << endl;
+    xml << "\t<!ELEMENT Directors (Director*)>\n\t\t<!ELEMENT Director EMPTY>" << endl;
+    xml << "\t\t\t<!ATTLIST Director Id CDATA #REQUIRED>\n\t\t\t<!ATTLIST Director Name CDATA #REQUIRED>" << endl;
+    xml << "\t<!ELEMENT Actors (Actor*)>\n\t\t<!ELEMENT Actor EMPTY>" << endl;
+    xml << "\t\t\t<!ATTLIST Actor Id CDATA #REQUIRED>\n\t\t\t<!ATTLIST Actor ActName CDATA #REQUIRED>\n\t\t\t<!ATTLIST Actor CharName CDATA #REQUIRED>" << endl;
+    xml << "]>" << endl;
+
+
+	
+		
     xml << "<Films>"<<endl;
     while(getline(csv, line)){
         Film tmp;
